dd48bfd4585fe239674ca966498905cfdb582a32
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-06-03 21:21:17 +0900

    param

--- src/About.js
+++ src/About.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import qs from "qs";
 
-const About = () => {
-    return (
-        <div>
-            <h1>소개</h1>
-            <p>리액트 라우터 기초 실습 예제 프로젝트</p>
-        </div>
-    );
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const showDetail = query.detail === "true";
+
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>리액트 라우터 기초 실습 예제 프로젝트</p>
+      {showDetail && <p>detail 값을 true로 설정했군요.</p>}
+    </div>
+  );
 };
 
 export default About;
