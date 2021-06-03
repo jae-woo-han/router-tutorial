e801fc256633dd6edb98a603bbdcaee342c77327
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-06-03 21:47:01 +0900

    sub route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
   return (
@@ -15,15 +15,13 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/jw">jw 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
